f3800e7a7bce920fe465f1ec5fca29c3fa98c7ce
Mhuzvar <huzvamat@fel.cvut.cz>
2025-06-24 14:06:27 +0200

    250624 added sources, progressed with A-weight filter approximation

--- losslib/wfilters.py
+++ losslib/wfilters.py
@@ -13,6 +13,14 @@ def Acurve(nvals=1024, fs=44100, mode=0):
     outputs:
         vector of magnitudes
     """
+    
+    """
+    1. get magnitude response
+    2. mirror around fs/2
+    3. calculate filter coeffs 0..M using formula 7.30 from book
+    4. calculate the rest of the coefficients using formula 7.31 (from the book)
+    """
+    
     match mode:
         case 0:
             fd = (31.5, 63, 100, 200, 400, 800, 1000, 2000, 3150, 4000, 5000,
@@ -21,13 +29,30 @@ def Acurve(nvals=1024, fs=44100, mode=0):
                   12.2, 12, 11.4, 10.1, 8.1, 0, -5.3, -11.7, -22.2, -42.7)
         case _:
             raise Exception(f"Wrong mode '{mode}' provided!")
-    fn = np.linspace(0,22050,22050)
-    rn = np.interp(fn, np.insert(np.asarray(fd), 0, 0), np.insert(np.asarray(rd),0 ,-60))
+    fn = np.linspace(1,22050,22050)
+    fnlog = np.log10(fn)
+    
+    fdlog = np.log10(np.asarray(fd))
+    fdlog = np.insert(fdlog, 0, fdlog[0]-30)
+    rdmod = np.insert(np.asarray(rd), 0, rd[0]-600)
+    rnlog = np.interp(fnlog, fdlog, rdmod)
+    rnlin = np.power(10, rnlog/10)
+
     plt.semilogx(fd, rd)
+    plt.xlim([20, 2e4])
+    plt.ylim([-50,20])
     plt.grid()
     plt.show()
-    plt.semilogx(fn,rn)
+    plt.semilogx(fn,rnlog)
+    plt.xlim([20, 2e4])
+    plt.ylim([-50,20])
+    plt.grid()
     plt.show()
+    plt.plot(fn,rnlin)
+    plt.xlim([20, 2e4])
+    plt.grid()
+    plt.show()
+    
 
 
 if __name__=="__main__":
